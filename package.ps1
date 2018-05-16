c2ed2c987fc1e3b79d53103b603933bf473f8e0a
Ted John <ted@brambles.org>
2018-05-16 12:31:25 +0100

    Use zip to package objects on unix
    Compress-Archive was not adding any file permissions.

--- package.ps1
+++ package.ps1
@@ -1,5 +1,14 @@
 #!/bin/pwsh
+$ErrorActionPreference = "Stop"
+
 # Create zip archive containing objects
+$useZip = $false
+if (Get-Command "zip" -ErrorAction SilentlyContinue)
+{
+    # Use zip if possible as it handles permissions better on unix
+    $useZip = $true
+    Write-Host "Using zip instead of Compress-Archive"
+}
 
 Write-Host -ForegroundColor Cyan "Re-creating artifacts directory..."
 Remove-Item -Force -Recurse artifacts -ErrorAction SilentlyContinue
@@ -15,16 +24,43 @@ foreach ($d in Get-ChildItem -Directory -Recurse artifacts/objects)
 {
     if (Test-Path (Join-Path $d.FullName object.json))
     {
+        $objName = $d.Name
         $src = Resolve-Path -Relative $d.FullName
         $dst = $src + ".parkobj"
         Write-Host "$src -> $dst"
-        # We must use .zip extension for Compress-Archive to work
-        Compress-Archive -Force "$src/*" -DestinationPath ($dst + ".zip") -CompressionLevel Optimal
-        Move-Item ($dst + ".zip") $dst
-        Remove-Item -Force -Recurse $src
+        if ($useZip)
+        {
+            Push-Location $src
+                zip -r9 "../$objName.parkobj" (Get-ChildItem).Name
+                if ($LASTEXITCODE -ne 0)
+                {
+                    throw "zip failed with $LASTEXITCODE"
+                }
+            Pop-Location
+        }
+        else
+        {
+            # We must use .zip extension for Compress-Archive to work
+            Compress-Archive -Force "$src/*" -DestinationPath ($dst + ".zip") -CompressionLevel Optimal
+            Move-Item ($dst + ".zip") $dst
+        }
+        Remove-Item -Force -Recurse $d.FullName
     }
 }
 
 Write-Host -ForegroundColor Cyan "Creating final archive..."
-Compress-Archive -Force "artifacts/objects/*" -DestinationPath "artifacts/objects.zip" -CompressionLevel Optimal
+if ($useZip)
+{
+    Push-Location "artifacts/objects"
+        zip -r9 "../objects.zip" (Get-ChildItem).Name
+        if ($LASTEXITCODE -ne 0)
+        {
+            throw "zip failed with $LASTEXITCODE"
+        }
+    Pop-Location
+}
+else
+{
+    Compress-Archive -Force "artifacts/objects/*" -DestinationPath "artifacts/objects.zip" -CompressionLevel Optimal
+}
 Remove-Item -Force -Recurse artifacts/objects
