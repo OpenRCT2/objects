be70d01b230e27100a5095312d0d82ab814b5856
Tulio Leao <tupaschoal@gmail.com>
2020-04-08 20:07:08 -0300

    Fix #77: Extract language_dump.py logic into functions

--- language_dump.py
+++ language_dump.py
@@ -1,37 +1,41 @@
 #!/usr/bin/env python3
+"""
+Dump translations for OpenRCT2\'s JSON objects given a language and a file
+"""
 
 import argparse
 import glob
 import json
 # from pprint import pprint
 
-supported_languages = ["ar-EG", "ca-ES", "cs-CZ", "da-DK", "de-DE", "en-GB", "en-US", "es-ES", "fi-FI",\
-                       "fr-FR", "hu-HU", "it-IT", "ja-JP", "ko-KR", "nb-NO", "nl-NL", "pl-PL", "pt-BR",\
-                       "ru-RU", "sv-SE", "tr-TR", "zh-CN", "zh-TW"]
+SUPPORTED_LANGUAGES = ["ar-EG", "ca-ES", "cs-CZ", "da-DK", "de-DE", "en-GB", "en-US", "es-ES",\
+                       "fi-FI", "fr-FR", "hu-HU", "it-IT", "ja-JP", "ko-KR", "nb-NO", "nl-NL",\
+                       "pl-PL", "pt-BR", "ru-RU", "sv-SE", "tr-TR", "zh-CN", "zh-TW"]
 
-# Command line arguments.
-parser = argparse.ArgumentParser(description='Dump translations for OpenRCT2\'s JSON objects.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('-o', '--objects', default="objects", help='JSON objects directory')
-parser.add_argument('-f', '--fallback', default="en-GB", help='Fallback language to check against', choices=supported_languages)
-parser.add_argument('-d', '--dumpfile', help='Translation file to export to', required=True)
-parser.add_argument('-l', '--language', help='Language that should be extracted, e.g. ja-JP', required=True, choices=supported_languages)
-parser.add_argument('-v', '--verbose', action='store_true', default=False, help='Maximize information printed on screen')
-args = parser.parse_args()
+def get_arg_parser():
+    """ Command line arguments """
+    parser = argparse.ArgumentParser(description='Dump translations for OpenRCT2\'s JSON objects.',\
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-o', '--objects', default="objects", help='JSON objects directory')
+    parser.add_argument('-f', '--fallback', default="en-GB",\
+                        help='Fallback language to check against', choices=SUPPORTED_LANGUAGES)
+    parser.add_argument('-d', '--dumpfile', help='Translation file to export to', required=True)
+    parser.add_argument('-l', '--language', help='Language that should be extracted, e.g. ja-JP',\
+                        required=True, choices=SUPPORTED_LANGUAGES)
+    parser.add_argument('-v', '--verbose', action='store_true', default=False,\
+                        help='Maximize information printed on screen')
+    return parser
 
-language_to_extract = args.language
-fallback_language = args.fallback
-dump_file_name = args.dumpfile
-verbose = args.verbose
+def extract_translations(language_to_extract, fallback_language, verbose, filename,\
+                         strings_by_object):
+    """ Read JSON file and extracts translations for a given language """
 
-strings_by_object = {}
-
-for filename in glob.iglob(args.objects + '/**/*.json', recursive=True):
     with open(filename, encoding="utf8") as file:
         data = json.load(file)
 
         if not 'strings' in data:
             print("No strings in " + data['id'] + ", skipping")
-            continue
+            return
 
         for string_key in data['strings']:
             if not data['id'] in strings_by_object:
@@ -40,16 +44,36 @@ for filename in glob.iglob(args.objects + '/**/*.json', recursive=True):
             if language_to_extract in data['strings'][string_key]:
                 if verbose:
                     print("Found existing translation for " + data['id'])
-                strings_by_object[data['id']][string_key] = data['strings'][string_key][language_to_extract]
+                strings_by_object[data['id']][string_key] = \
+                    data['strings'][string_key][language_to_extract]
             elif fallback_language in data['strings'][string_key]:
-                print("No existing translation for " + data['id'] + " yet, using English")
-                strings_by_object[data['id']][string_key] = data['strings'][string_key][fallback_language]
+                print("No existing translation for " + data['id'] + \
+                      " yet, using English")
+                strings_by_object[data['id']][string_key] = \
+                    data['strings'][string_key][fallback_language]
             else:
                 if verbose:
-                    print("No existing translation for " + data['id'] + " yet, but no English string either -- skipping")
-                # pprint(data)
+                    print("No existing translation for " + data['id'] + \
+                          " yet, but no English string either -- skipping")
 
-out = open(dump_file_name, "w", encoding="utf8")
-json.dump(strings_by_object, out, indent=4, ensure_ascii=False, separators=(',', ': '))
-out.write("\n")
-out.close()
+def dump_translations():
+    """ Dump translations for OpenRCT2\'s JSON objects """
+    parser = get_arg_parser()
+    args = parser.parse_args()
+    language_to_extract = args.language
+    fallback_language = args.fallback
+    verbose = args.verbose
+    strings_by_object = {}
+
+    for filename in glob.iglob(args.objects + '/**/*.json', recursive=True):
+        extract_translations(language_to_extract, fallback_language, verbose,\
+                             filename, strings_by_object)
+
+    dump_file_name = args.dumpfile
+    out = open(dump_file_name, "w", encoding="utf8")
+    json.dump(strings_by_object, out, indent=4, ensure_ascii=False, separators=(',', ': '))
+    out.write("\n")
+    out.close()
+
+if __name__ == "__main__":
+    dump_translations()
