f623ad0bf13acabacde3f841e4ed8554d15517f2
Tulio Leao <tupaschoal@gmail.com>
2020-04-09 21:56:27 -0300

    Fix #79: Report language translation completeness on language_dump.py

--- language_dump.py
+++ language_dump.py
@@ -30,12 +30,14 @@ def extract_translations(language_to_extract, fallback_language, verbose, filena
                          strings_by_object):
     """ Read JSON file and extracts translations for a given language """
 
+    reference_str_count = 0
+    translated_str_count = 0
     with open(filename, encoding="utf8") as file:
         data = json.load(file)
 
         if not 'strings' in data:
             print(f"No strings in {data['id']}, skipping")
-            return
+            return (translated_str_count, reference_str_count)
 
         for string_key in data['strings']:
             if not data['id'] in strings_by_object:
@@ -46,14 +48,18 @@ def extract_translations(language_to_extract, fallback_language, verbose, filena
                     print(f"Found existing translation for {data['id']}")
                 current_translation = data['strings'][string_key][language_to_extract]
                 strings_by_object[data['id']][string_key] = current_translation
+                reference_str_count += 1
+                translated_str_count += 1
             elif fallback_language in data['strings'][string_key]:
                 print(f"No existing translation for {data['id']} yet, using {fallback_language}")
                 fallback_translation = data['strings'][string_key][fallback_language]
                 strings_by_object[data['id']][string_key] = fallback_translation
+                reference_str_count += 1
             else:
                 if verbose:
                     print(f"No existing translation for {data['id']} yet,"
                           f" but no {fallback_language} string either -- skipping")
+    return (translated_str_count, reference_str_count)
 
 def dump_translations():
     """ Dump translations for OpenRCT2's JSON objects """
@@ -63,10 +69,18 @@ def dump_translations():
     fallback_language = args.fallback
     verbose = args.verbose
     strings_by_object = {}
+    reference_str_count = 0
+    translated_str_count = 0
 
     for filename in glob.iglob(args.objects + '/**/*.json', recursive=True):
-        extract_translations(language_to_extract, fallback_language, verbose,\
-                             filename, strings_by_object)
+        obj_translations, ref_obj_translation =\
+            extract_translations(language_to_extract, fallback_language, verbose,\
+                                 filename, strings_by_object)
+        reference_str_count += ref_obj_translation
+        translated_str_count += obj_translations
+
+    translation_progress = round(100*translated_str_count/reference_str_count, 2)
+    print(f'{language_to_extract}: {translation_progress}% completeness')
 
     dump_file_name = args.dumpfile
     out = open(dump_file_name, "w", encoding="utf8")
