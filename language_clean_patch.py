6e6663f601a193ff1cd6174f27b0b33537ad0f65
Aaron van Geffen <aaron@aaronweb.net>
2020-10-24 16:09:20 +0200

    Pull SUPPORTED_LANGUAGES from one file

--- language_clean_patch.py
+++ language_clean_patch.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 
+from languages import SUPPORTED_LANGUAGES
 from unidiff import PatchSet
 from unidiff.constants import LINE_TYPE_EMPTY
 
 import argparse
 import os
-
-SUPPORTED_LANGUAGES = ["ar-EG", "ca-ES", "cs-CZ", "da-DK", "de-DE", "en-GB", "en-US", "es-ES",\
-                       "fi-FI", "fr-FR", "hu-HU", "it-IT", "ja-JP", "ko-KR", "nb-NO", "nl-NL",\
-                       "pl-PL", "pt-BR", "ru-RU", "sv-SE", "tr-TR", "zh-CN", "zh-TW"]
 
 def dir_path(string):
     """ Checks for a valid dir_path """
---
+++ languages.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+SUPPORTED_LANGUAGES = ["ar-EG", "ca-ES", "cs-CZ", "da-DK", "de-DE", "en-GB", "en-US", "es-ES",\
+                       "fi-FI", "fr-FR", "hu-HU", "it-IT", "ja-JP", "ko-KR", "nb-NO", "nl-NL",\
+                       "pl-PL", "pt-BR", "ru-RU", "sv-SE", "tr-TR", "zh-CN", "zh-TW"]
