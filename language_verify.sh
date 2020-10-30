0f2d3ec1ac68d9effbc0303ba6057b6ea0965229
Aaron van Geffen <aaron@aaronweb.net>
2020-10-30 22:44:07 +0100

    Squash: whitespace

--- language_verify.sh
+++ language_verify.sh
@@ -6,7 +6,7 @@ set -e
 # We will need some directions... Let's make sure we get them.
 if [ $# -ne 1 ] || [ ! -d $1 ];
     then echo "Usage: $0 <directory>"
-	exit
+    exit
 fi
 
 # Traverse input directory, and verify syntax in all files is correct.
