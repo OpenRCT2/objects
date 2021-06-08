ef0b9e8c9a946e2e9bf699b92640c3ad9ecf0eb5
Michael Steenbeek <m.o.steenbeek@gmail.com>
2021-06-08 19:27:12 +0200

    Change eo-OO to eo-ZZ

--- languages.py
+++ languages.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
-SUPPORTED_LANGUAGES = ["ar-EG", "ca-ES", "cs-CZ", "da-DK", "de-DE", "en-GB", "en-US", "eo-OO",\
+SUPPORTED_LANGUAGES = ["ar-EG", "ca-ES", "cs-CZ", "da-DK", "de-DE", "en-GB", "en-US", "eo-ZZ",\
                        "es-ES", "fi-FI", "fr-FR", "hu-HU", "it-IT", "ja-JP", "ko-KR", "nb-NO",\
                        "nl-NL", "pl-PL", "pt-BR", "ru-RU", "sv-SE", "tr-TR", "zh-CN", "zh-TW"]
