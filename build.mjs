ee19cce0b137b030a2d7399aee02414440eb1407
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2022-06-15 21:17:19 +0100

    Get almost working

--- build.mjs
+++ build.mjs
@@ -4,6 +4,8 @@ import path from 'path';
 import { spawn } from 'child_process';
 import { platform } from 'os';
 
+const verbose = process.argv.indexOf('--verbose') != -1;
+
 async function main() {
     // fs.cpSync('objects', 'artifacts', {recursive: true});
     const objects = await getObjects('artifacts');
@@ -14,14 +16,17 @@ async function main() {
 	}
 	var requiresProcessing = true;
         if (typeof images[Symbol.iterator] === 'function') {
-	    for (const image of images) {
+	    if (images.length === 0) requiresProcessing = false;
+            for (const image of images) {
                 if (typeof image === 'string') {
                     requiresProcessing = false;
 		}    
 	    }
-        }
+        } else {
+            requiresProcessing = false;
+	}
 	if (requiresProcessing) {
-            console.log(`Reprocess ${obj.id}`);
+            reprocessObject(obj);
 	}
     }
 }
@@ -32,13 +37,36 @@ async function getObjects(dir) {
     for (const file of files) {
         const jsonRegex = /^.+\..+\.json$/;
         if (jsonRegex.test(file)) {
-            const cwd = path.join('..', path.dirname(file));
+            const cwd = path.dirname(file);
             const obj = await readJsonFile(file);
             obj.cwd = cwd;
             result.push(obj);
         }
     }
     return result;
+}
+
+async function reprocessObject(obj) {
+    console.log(`Reprocessing ${obj.id}`);
+    //startProcess(path.join(process.cwd(),'gxc'), [''],obj.cwd);
+    await writeJsonFile(path.join(obj.cwd, 'images.json'), obj.images);
+    await compileGx(obj.cwd, 'images.json', 'images.dat');
+    var imageCount = await getGxImageCount(obj.cwd, 'images.dat');
+    console.log(`Num Images ${imageCount}`);
+}
+
+function compileGx(cwd, manifest, outputFile) {
+    return startProcess(path.join(process.cwd(), 'gxc'), ['build', outputFile, manifest], cwd);
+}
+
+async function getGxImageCount(cwd, inputFile) {
+    const stdout = await startProcess(path.join(process.cwd(), 'gxc'), ['details', inputFile], cwd);
+    const result = stdout.match(/numEntries: ([0-9]+)/);
+    if (result) {
+        return parseInt(result[1]);
+    } else {
+        throw new Error(`Unable to get number of images for gx file: ${inputFile}`);
+    }
 }
 
 function readJsonFile(path) {
@@ -103,6 +131,38 @@ function getAllFiles(root) {
             });
         };
         find(root);
+    });
+}
+
+function startProcess(name, args, cwd) {
+    return new Promise((resolve, reject) => {
+        const options = {};
+        if (cwd) options.cwd = cwd;
+        if (verbose) {
+            console.log(`Launching \"${name} ${cwd} ${args.join(' ')}\"`);
+        }
+        const child = spawn(name, args, options);
+        let stdout = '';
+        child.stdout.on('data', data => {
+            stdout += data;
+        });
+        child.stderr.on('data', data => {
+            stdout += data;
+        });
+        child.on('error', err => {
+            if (err.code == 'ENOENT') {
+                reject(new Error(`${name} was not found`));
+            } else {
+                reject(err);
+            }
+        });
+        child.on('close', code => {
+            if (code !== 0) {
+                reject(new Error(`${name} failed:\n${stdout}`));
+            } else {
+                resolve(stdout);
+            }
+        });
     });
 }
 
