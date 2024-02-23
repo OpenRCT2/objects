ee1e69ab3c6450ea5c49a47dc3ee088d547a6b7a
Tulio Leao <tupaschoal@gmail.com>
2024-02-23 19:44:37 -0300

    Add uk-UA as supported language

--- languages.py
+++ languages.py
@@ -2,4 +2,5 @@
 
 SUPPORTED_LANGUAGES = ["ar-EG", "ca-ES", "cs-CZ", "da-DK", "de-DE", "en-GB", "en-US", "eo-ZZ",\
                        "es-ES", "fi-FI", "fr-FR", "hu-HU", "it-IT", "ja-JP", "ko-KR", "nb-NO",\
-                       "nl-NL", "pl-PL", "pt-BR", "ru-RU", "sv-SE", "tr-TR", "zh-CN", "zh-TW"]
+                       "nl-NL", "pl-PL", "pt-BR", "ru-RU", "sv-SE", "tr-TR", "uk-UA", "zh-CN",\
+                       "zh-TW"]
